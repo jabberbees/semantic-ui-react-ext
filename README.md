5648b481668a55fada1631235b7f9beb84ae1083
Emmanuel Boutin <emmanuel.boutin@jabberbees.com>
2019-03-26 09:45:21 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -10,21 +10,55 @@
 npm install --save semantic-ui-react-ext
 ```
 
-## Usage
+## Components
+
+### FormField
+
+The FormField component conveniently combines several Semantic UI React components:
+  * a Label
+  * a form control (an Input component by default)
+  * an error label
+
+The embedded control is specified using the 'control' prop.
+
+The 'error' prop (string) specifies the error label message and wether the error label is displayed or not.
 
 ```jsx
 import React, { Component } from 'react'
 
-import MyComponent from 'semantic-ui-react-ext'
+import { Form, Checkbox } from 'semantic-ui-react';
+
+import FormField from 'semantic-ui-react-ext';
 
 class Example extends Component {
   render () {
     return (
-      <MyComponent />
+      <Form>
+        <FormField
+          label='First Name'
+          required
+        />
+        <FormField
+          label='Last Name'
+          required
+        />
+        <FormField
+          control={Checkbox}
+          label='I consent to everything'
+          defaultIndeterminate={true}
+          required
+        />
+      </Form>
     )
   }
 }
 ```
+
+## Example
+
+Screenshot from included example:
+
+[[https://github.com/jabberbees/semantic-ui-react-ext/blob/master/doc/example-screenshot.png|alt=Example]]
 
 ## License
 
--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,106 @@
-import React, { Component } from 'react'
-
-import ExampleComponent from 'semantic-ui-react-ext'
+import React, { Component } from 'react';
+import {
+  Container, Header, Segment, Form,
+  Checkbox, Dropdown, Radio, Divider, TextArea
+} from 'semantic-ui-react';
+import { FormField } from 'semantic-ui-react-ext';
 
 export default class App extends Component {
-  render () {
+  state = {}
+
+  handleChange = (e, { value }) => this.setState({ value })
+
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
+      <React.Fragment>
+        <Header as="h1" style={{textAlign: 'center'}}>Semantic UI React Ext</Header>
+        <Container style={{ paddingTop: 0 }}>
+          <Segment attached>
+            <Form>
+              <FormField
+                label='First Name'
+                required
+              />
+              <FormField
+                label='Last Name'
+                required
+                error='Invalid last name'
+              />
+              <FormField
+                label='I consent to everything'
+                control={Checkbox}
+                defaultIndeterminate={true}
+              />
+              <FormField
+                label='Use maximum power'
+                control={Checkbox}
+                slider
+                defaultChecked={true}
+                error='You do not have enough energy'
+              />
+
+              <Divider />
+
+              <FormField
+                label='Choice'
+                control={Dropdown}
+                placeholder='Your choice'
+                fluid
+                required
+                selection
+                options={[
+                  {
+                    key: 'one',
+                    text: 'One',
+                    value: 1
+                  },
+                  {
+                    key: 'two',
+                    text: 'Two',
+                    value: 2
+                  },
+                  {
+                    key: 'three',
+                    text: 'Three',
+                    value: 3
+                  }
+                ]}
+              />
+
+              <Divider />
+
+              <Form.Field>
+                Selected value: <b>{this.state.value}</b>
+              </Form.Field>
+              <FormField
+                control={Radio}
+                label='Choose this'
+                name='radioGroup'
+                value='this'
+                checked={this.state.value === 'this'}
+                onChange={this.handleChange}
+              />
+              <FormField
+                control={Radio}
+                label='Or that'
+                name='radioGroup'
+                value='that'
+                checked={this.state.value === 'that'}
+                onChange={this.handleChange}
+                error={this.state.value === 'that' ? 'Not allowed' : ''}
+              />
+
+              <Divider />
+
+              <FormField
+                control={TextArea}
+                label='Tell us the story of your life'
+                required
+              />
+            </Form>
+          </Segment>
+        </Container>
+      </React.Fragment>
     )
   }
 }
